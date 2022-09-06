c0af66a2617758c312f3417cb6f5fec415e61f2c
CrisFRC <ing.cristian.roac@gmail.com>
2022-09-05 22:10:51 -0500

    inicio del desarrollo

--- src/test/java/smart_run/SmartRunner.java
+++ src/test/java/smart_run/SmartRunner.java
@@ -1,4 +1,16 @@
 package smart_run;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
 public class SmartRunner {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+
+
 }
